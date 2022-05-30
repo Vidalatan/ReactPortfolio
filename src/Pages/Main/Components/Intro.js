be2129419304d058b4d4ee154a31f03a60e6617c
Vidalatan <vidalatan@gmail.com>
2022-05-30 18:56:52 -0500

    Greeter typer
    Next is to make it execute when page loads.

--- src/Pages/Main/Components/Intro.js
+++ src/Pages/Main/Components/Intro.js
@@ -5,6 +5,12 @@ import classify from '../../../utils/classify';
 import classes from '../Main.module.css';
 
 export default function Intro() {
+  // Timing Settings (for convenience)
+    const highlightInterval = 50;
+    const typingInterval = 100;
+    const delayInterval = 1000;
+  // ---------------------------------
+
   const {currentStyle} = useContext(ThemeContext);
 
   const greeterMsgE = 'Hello, it\'s nice to meet you...';
@@ -12,14 +18,56 @@ export default function Intro() {
 
   function makeSpanSequence(msg) {
     const _ = msg.split('');
-    return _.map((char, i = 0) => (<span key={i} className={`typerChar_${i}`}>{char}</span>))
+    return _.map((char, i = 0) => (<span key={i}>{char}</span>))
   }
 
-  const [currentGreetMsg, setCurrentGreetMsg] = useState(makeSpanSequence(greeterMsgE))
+  const [currentGreetMsg, setCurrentGreetMsg] = useState(makeSpanSequence(greeterMsgC))
+
+  function greeterTyper() {
+    function highlighter() {
+      let index = currentGreetMsg.length
+      const highTime = setInterval(() => {
+        if (index <= 0) {
+          clearInterval(highTime)
+          setTimeout(clearGreeting, delayInterval);
+        } else {
+          setCurrentGreetMsg(prev => {
+            prev.splice(index, 1, (<span key={index} className={classes.typerH}>{currentGreetMsg[index].props.children}</span>))
+            return [...prev]
+          })
+          index--
+        }
+      }, highlightInterval);
+    }
+
+    function clearGreeting() {
+      setCurrentGreetMsg([""])
+      setTimeout(typeNew, delayInterval)
+    }
+
+    function typeNew() {
+      const newMessage = makeSpanSequence(greeterMsgE);
+      let index = -1;
+      const typeTime = setInterval(() => {
+        if (index >= newMessage.length){
+          clearInterval(typeTime)
+        } else {
+          setCurrentGreetMsg(prev => {
+            console.log(index);
+            const _ = [...prev, newMessage[index]]
+            return _
+          })
+          index++
+        }
+      }, typingInterval);
+    }
+
+    highlighter()
+  }
 
   return (
     <>
-    <h2 className={classes.typerGreeting}>{currentGreetMsg}</h2>
+    <h2 className={classes.typerGreeting} onClick={greeterTyper}>{currentGreetMsg}</h2>
 
     <p>I am a full-stack web developer focusing on UI/UX design.</p>
     
