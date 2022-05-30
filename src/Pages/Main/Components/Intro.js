292baafefddd7118b3211a51418f2b2cd0e03278
Vidalatan <vidalatan@gmail.com>
2022-05-30 14:15:14 -0500

    Set up 'spanned' greeting

--- src/Pages/Main/Components/Intro.js
+++ src/Pages/Main/Components/Intro.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {useContext, useEffect, useState} from 'react';
+import { ThemeContext } from '../../../ContextProviders/Theme/ThemeContext';
+import classify from '../../../utils/classify';
+
+import classes from '../Main.module.css';
 
 export default function Intro() {
+  const {currentStyle} = useContext(ThemeContext);
+
+  const greeterMsgE = 'Hello, it\'s nice to meet you...';
+  const greeterMsgC = '你好, 很高兴认识你...';
+
+  function makeSpanSequence(msg) {
+    const _ = msg.split('');
+    return _.map((char, i = 0) => (<span key={i} className={`typerChar_${i}`}>{char}</span>))
+  }
+
+  const [currentGreetMsg, setCurrentGreetMsg] = useState(makeSpanSequence(greeterMsgE))
+
   return (
     <>
-    <h2>Hello, it's nice to meet you...</h2>
+    <h2 className={classes.typerGreeting}>{currentGreetMsg}</h2>
 
     <p>I am a full-stack web developer focusing on UI/UX design.</p>
     
