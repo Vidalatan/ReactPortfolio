4e30524813ba8337c5b90406a52814d4925fab36
Vidalatan <vidalatan@gmail.com>
2022-06-03 02:25:15 -0500

    Projects and images updated

---
+++ src/Pages/GlobalComponents/ProjectsCube/Projects.js
@@ -0,0 +1,53 @@
+import pokeshop from './assets/pokeshop.png';
+import dndyImgL from './assets/dndyLight.png';
+import dndyImgD from './assets/dndyDark.png';
+import nmn from './assets/NMN.png';
+import opticCalcL from './assets/opticCalcLight.png';
+import opticCalcD from './assets/opticCalcDark.png';
+import readmeL from './assets/readmeLight.png';
+import readmeD from './assets/readmeDark.png';
+import weatherL from './assets/weatherDashboardLight.png';
+import weatherD from './assets/weatherDashboardDark.png';
+
+
+const Projects = 
+{
+  "0": 
+  {
+    "img": pokeshop,
+    "darkImg": null,
+    "repoURL": "https://github.com/Vidalatan/poke-shop"
+  },
+  "1": 
+  {
+    "img": dndyImgL,
+    "darkImg": dndyImgD,
+    "repoURL": "https://github.com/DNDYArt/DNDYArt"
+  },
+  "2": 
+  {
+    "img": nmn,
+    "darkImg": null,
+    "repoURL": "https://github.com/Nerds-Meet-Nerds/Nerds-Meet-Nerds"
+  },
+  "3": 
+  {
+    "img": opticCalcL,
+    "darkImg": opticCalcD,
+    "repoURL": "https://github.com/Vidalatan/Optic-Calculator"
+  },
+  "4": 
+  {
+    "img": readmeL,
+    "darkImg": readmeD,
+    "repoURL": "https://github.com/Vidalatan/readme_maker"
+  },
+  "5": 
+  {
+    "img": weatherL,
+    "darkImg": weatherD,
+    "repoURL": "https://github.com/Vidalatan/weather_dashboard"
+  }
+}
+
+export default Projects;
